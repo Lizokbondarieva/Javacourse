c40a8d24166bc293453437340e0d7d4d282b6ada
Lizokbondarieva <lizokbondarieva@gmail.com>
2025-02-19 17:30:22 +0200

    animals

---
+++ src/homework1/Animals.java
@@ -0,0 +1,46 @@
+package homework1;
+class cat{
+    int paw;
+    int tail;
+}
+class fish{
+    String location;
+    String name;
+}
+class hamster{
+    double weight;
+    int age;
+}
+class dog{
+     String breed;
+     String nickname;
+}
+class horse{
+    int speed;
+    int power;
+}
+class parrot{
+    String color;
+    boolean speak;
+}
+class giraffe{
+    int height;
+    String sex;
+}
+class elephant{
+    int trunk;
+    int children;
+}
+class monkey{
+    String home;
+    String food;
+}
+class koala{
+    int sleep;
+    int cute;
+}
+public class Animals {
+    public static void main(String[] args){
+    System.out.println("10 класів тваринок");
+    }
+}
